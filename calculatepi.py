6a1a5e1e0c22b0771c0f63cb1f0a3713c62eed07
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-09-13 13:56:03 -0400

    Updated from Brython Server: 9/13/2016 1:56:03 PM

--- calculatepi.py
+++ calculatepi.py
@@ -22,9 +22,9 @@ Note: remember that the printed value of pi will be an estimate!
 
 """
 n = input ("I will estimate pi. How many terms should I use? ")
-d=input ("How many decimal places should I use in the result? ")
+decimals=input ("How many decimal places should I use in the result? ")
 intlist = range(0, int(n))
 pi = lambda n: (4*((-1)**n)/((2*n)+1))
 pidigits = [pi(x) for x in list(intlist)]
-PI = sum(pidigits)
-print("The approximate value of pi is "+format.round(PI,d))
+PI = int(sum(pidigits))
+print("The approximate value of pi is {0}".format(round(PI, decimals)))
